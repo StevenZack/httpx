8a661325802d832a58c12b5153e13ae7798d99bb
stevenzack <stevenzack@qq.com>
2019-07-03 11:36:12 +0800

    Just a Backup

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
-package io.github.stevenzack;
-
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("Hello World!");
     }
 }
